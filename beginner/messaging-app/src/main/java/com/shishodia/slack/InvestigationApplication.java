7e9cc9202dc6c481fac3fe583c438f3906828ca6
shivamshishodia <theshishodia@gmail.com>
2022-12-28 01:03:15 +0530

    Added Block Action and Chart

--- beginner/messaging-app/src/main/java/com/shishodia/slack/InvestigationApplication.java
+++ beginner/messaging-app/src/main/java/com/shishodia/slack/InvestigationApplication.java
@@ -5,15 +5,19 @@ import com.slack.api.bolt.AppConfig;
 import com.slack.api.bolt.socket_mode.SocketModeApp;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import com.slack.api.methods.response.views.ViewsOpenResponse;
 import com.slack.api.model.block.composition.OptionObject;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import com.slack.api.model.view.View;
 
 import static com.slack.api.model.view.Views.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 
@@ -73,15 +77,78 @@ public class InvestigationApplication {
 									.text(markdownText(mt -> mt.text("Initiate an investigation")))
 									.accessory(button(btn -> btn
 											.text(plainText(pt -> pt.text("Create Investigation").emoji(true)))
-											.value("click_123").actionId("click-btn")))))));
+											.value("click_123").actionId("click-btn"))))
+			)));
 			ctx.client().viewsPublish(r -> r.userId(payload.getEvent().getUser()).view(appHomeView));
 			return ctx.ack();
 		});
 
-		// Command Enablement.
-		app.command("/hello", (req, ctx) -> {
-			return ctx.ack(":wave: Hello, " + req.getPayload().getUserName() + "!");
+		app.blockAction("click-btn", (req, ctx) -> {
+			// Do something where
+			View appHomeView = view(view -> view
+			.type("modal")
+			.callbackId("slack-view-sample")
+			.title(viewTitle(title -> title.type("plain_text").text("Slack View").emoji(true)))
+			.submit(viewSubmit(submit -> submit.type("plain_text").text("Submit").emoji(true)))
+			.close(viewClose(close -> close.type("plain_text").text("Cancel").emoji(true)))
+			.blocks(asBlocks(
+					section(section -> section
+							.text(markdownText(mt -> mt.text("*Hi Shivam, Welcome to your personal space*")))),
+					divider(),
+					section(section -> section.text(markdownText(mt -> mt.text(
+							"You can create your own investigations and execute queries from this home page. For more details please refer <https://confluence.oci.oraclecorp.com/display/LOGAN/Investigations+-+proposed+design+in+OCI>."))))
+			)));
+			ViewsOpenResponse vw = ctx.client().viewsOpen(r -> r
+					.triggerId(ctx.getTriggerId())
+					.view(appHomeView));
+			return ctx.ack();
 		});
+
+		// Modal Enablement.
+		app.command("/modal", (req, ctx) -> {
+			return ctx.ack();
+		});
+
+		// Command Enablement.
+		app.command("/chart", (req, ctx) -> {
+            var logger = ctx.logger;
+            try {
+                var payload = req.getPayload();
+				String chartType;
+				switch (payload.getText()) {
+					case "data":
+					chartType = "chart-" + payload.getText() + ".png";
+					break;
+					case "histogram":
+					chartType = "chart-" + payload.getText() + ".jpeg";
+					break;
+					case "pie":
+					chartType = "chart-" + payload.getText() + ".png";
+					break;
+					default:
+					chartType = "chart-data.png";
+					break;
+				}
+                // The name of the file you're going to upload
+                var filepath = "/Users/shivamshishodia/Desktop/Investigation/slack-api-concepts/beginner/messaging-app/src/main/java/com/shishodia/slack/resources/" + chartType;
+                // Call the files.upload method using the built-in WebClient
+                var result = ctx.client().filesUpload(r -> r
+                    // The token you used to initialize your app is stored in the `context` object
+                    // .token(ctx.getBotToken())
+                    .channels(Arrays.asList(payload.getChannelId()))
+                    .initialComment("Here's my file :smile:")
+                    // Include your filename in a ReadStream here
+					.filename(UUID.randomUUID().toString())
+                    .file(new File(filepath))
+                );
+                // Print result
+                logger.info("result: {}", result);
+            } catch (IOException | SlackApiException e) {
+                logger.error("error: {}", e.getMessage(), e);
+            }
+            // Acknowledge incoming command event
+            return ctx.ack();
+        });
 
 		// TODO: Global shortcuts.
 		app.globalShortcut("launch_query", (req, ctx) -> {
